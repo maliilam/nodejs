fc28ee6be047a2c0993dec0d1b44d11bd84de84d
maliilma <maliyoujian@gmail.com>
2020-11-03 11:31:23 +0200

    rendering the todo react app

---
+++ todo/public/precache-manifest.85779ad19e2b22f58f1411cf258fa5c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb4725f4de2bcc711f3e4398f10dc5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21cf46ebe9ebb01ec774",
+    "url": "/static/css/main.6c1aa801.chunk.css"
+  },
+  {
+    "revision": "c7c6561cb9c8d6011e3c",
+    "url": "/static/js/2.52fa8d95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.52fa8d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21cf46ebe9ebb01ec774",
+    "url": "/static/js/main.e35ab462.chunk.js"
+  },
+  {
+    "revision": "ed06388feb9206724577",
+    "url": "/static/js/runtime-main.e90f9dad.js"
+  }
+]);
